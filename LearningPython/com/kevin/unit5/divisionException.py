8f1f297df6d6fad03c89211979f5626b10fae5f1
KevinJavaLee <1401597760@qq.com>
2020-07-17 19:44:15 +0800

    Python复习学习第七天第二次提交

--- LearningPython/com/kevin/unit5/divisionException.py
+++ LearningPython/com/kevin/unit5/divisionException.py
@@ -5,12 +5,32 @@
 @Software: PyCharm
 """
 
-try:
-    first_name = input("请输入第一个数字:\n")
-    second_name = input("请输入第二个数字:\n")
-    num = int(first_name) / int(second_name)
-except ZeroDivisionError:
-    print("除数不能为零")
-else:
+# try:
+#     first_name = input("请输入第一个数字:\n")
+#     second_name = input("请输入第二个数字:\n")
+#     num = int(first_name) / int(second_name)
+# except ZeroDivisionError:
+#     print("除数不能为零")
+# else:
+#
+#     print(num)
+#
 
-    print(num)
+print("请输入两个数字:")
+print("输入q退出!")
+
+while True:
+    first_num = input("请输入第一个数:")
+    if first_num == 'q':
+        break
+    second_num = input("请输入第二数：")
+
+    if second_num == 'q':
+        break
+    try:
+        sum = int(first_num) / int(second_num)
+    except ZeroDivisionError:
+        pass
+    else:
+        print(sum)
+
